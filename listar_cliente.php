c85a9060be265d48a389e6cf414e0e821be44aaa
Saul Kaique <e107ti@etec.sp.gov.br>
2023-06-26 21:32:05 -0300

    ultima parte feita com meu colega Joliano

--- listar_cliente.php
+++ listar_cliente.php
@@ -8,7 +8,7 @@
         <script src="https://kit.fontawesome.com/c0f408d1cc.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="sistema/style.css">
+        <link rel="stylesheet" href="cliente.css">
 </head>
 <body>
     <nav class="navbar navbar-dark bg-dark fixed-top">
@@ -58,7 +58,10 @@
                           </tr>
                         </thead>
                         <tbody>
-                          <?php
+
+
+                        
+                        <?php
                           include 'conexao.php';
                           $sql = "SELECT * FROM tb_cliente";
                           $pesquisa = mysqli_query($conexao,$sql);
@@ -79,6 +82,12 @@
                             </td>
                             </tr>
                          <?php }?>
+
+
+
+
+                        
+                         
                         </tbody>
                     </table>
                </div>
